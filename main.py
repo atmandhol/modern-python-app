60953c89be721ecf283d7dae5ab8f21560abdd85
Atman Dhol <atmandhol@hotmail.com>
2021-10-15 10:10:55 -0400

    Updated main, added setup.cfg for flake8 control

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-from flask import Flask, json, jsonify, make_response
+from flask import Flask, jsonify, make_response
 
 app = Flask(__name__)
+
 
 @app.route("/")
 def hello_world():
     return make_response(jsonify({"status": "OK"}))
-
